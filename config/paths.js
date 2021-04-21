1fe57063fbeaf98de0d86240fb5dbc27d434d6d8
eljouno <joper@smile.fr>
2021-04-21 09:33:28 +0200

    update webpack 5 and plugins ok 0 vulnnerabilities

--- config/paths.js
+++ config/paths.js
@@ -1,6 +1,7 @@
 const path = require('path')
 
 module.exports = {
+  root: path.resolve(__dirname, '../app'), // source files
   src: path.resolve(__dirname, '../app'), // source files
   build: path.resolve(__dirname, '../dist/'), // production build files
   assets: path.resolve(__dirname, '../app/assets'), // static files to copy to build folder
